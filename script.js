36dce8ac2c6255b65fac6f9ab929b602384e6e3c
fvfx <filipevfx3@gmail.com>
2024-06-03 22:40:41 -0300

    update

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 ﻿const mediaContainer = document.getElementById('media-container');
 let mediaData = [];
+let normalMedia = [];
+let sequentialMedia = [];
 let currentIndex = 0;
+let sequentialCounts = {};
 
 function loadMedia() {
     if (mediaData.length === 0) return;
 
     const currentMedia = mediaData[currentIndex];
-
     mediaContainer.innerHTML = ''; // Clear previous media
 
     if (currentMedia.type === 'image') {
@@ -73,12 +75,48 @@ function extractVimeoId(url) {
     return (match && match[1]) ? match[1] : null;
 }
 
+function organizeMediaData(data) {
+    normalMedia = data.filter(item => item.categoria === 'Normal');
+    sequentialMedia = data.filter(item => item.categoria === 'Sequencial');
+    sequentialCounts = {};
+
+    sequentialMedia.forEach(item => {
+        if (!sequentialCounts[item.repeatCount]) {
+            sequentialCounts[item.repeatCount] = [];
+        }
+        sequentialCounts[item.repeatCount].push(item);
+    });
+
+    createPlaybackSequence();
+}
+
+function createPlaybackSequence() {
+    mediaData = [];
+    let normalIndex = 0;
+    let sequentialIndex = {};
+
+    while (normalIndex < normalMedia.length) {
+        mediaData.push(normalMedia[normalIndex]);
+
+        Object.keys(sequentialCounts).forEach(count => {
+            if (!sequentialIndex[count]) sequentialIndex[count] = 0;
+            if ((normalIndex + 1) % count === 0) {
+                mediaData.push(...sequentialCounts[count]);
+                sequentialIndex[count]++;
+            }
+        });
+
+        normalIndex++;
+    }
+
+    startSlideshow();
+}
+
 function fetchMediaData() {
     fetch('programacao.json')
         .then(response => response.json())
         .then(data => {
-            mediaData = data.filter(item => item.categoria === 'Normal');
-            startSlideshow();
+            organizeMediaData(data);
         })
         .catch(error => console.error('Error fetching media data:', error));
 }
@@ -88,3 +126,4 @@ function startSlideshow() {
 }
 
 fetchMediaData();
+
